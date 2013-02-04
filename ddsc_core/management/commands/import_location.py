c8ee0b8ec0513323801ea335b84263fb4e5f15e1
shaoqing <shaoqing@ldam-fgslinux.(none)>
2013-02-04 17:44:43 +0100

    now the tree/leaves relation established

--- ddsc_core/management/commands/import_location.py
+++ ddsc_core/management/commands/import_location.py
@@ -15,10 +15,13 @@ from django.contrib.gis.geos import GEOSGeometry
 class Command(BaseCommand):
     args = '<CSV file>'
     help = 'Imports a CSV file of locations into the database.'
+    
+    
 
     def handle(self, *args, **options):
-        self.stdout.write('Foobar "%s"' % args[0])
         i = 1
+        maintain_list=[]
+        nr_0 = 0
         with open(args[0], 'rb') as f:
             reader = csv.reader(f)
             for row in reader:
@@ -30,33 +33,62 @@ class Command(BaseCommand):
                     z = row[5]
                     srid = row[6]
                     parentuuid = row[7]
+                    
+                    if parentuuid == 'N/A':
+                        maintain_list.append(0)
+                        nr_0+=1
+                    else :
+                        maintain_list.append(parentuuid)
+                    
                     description = row[10]
+                    
                     if row[11] is not '':
                         geometry_precision = row[11]
                     else:
                         geometry_precision = None
+                        
                     location_description = row[12]
                     location_type = row[13]
                     
                     if x is not '':
                         geo_input = 'POINT(' + x +' ' + y + ' ' + z +')'
-                        print geo_input
                         point_geometry = GEOSGeometry(geo_input, srid)
                     else :
                         point_geometry = ''
                         
                     path = i 
-                    i+=1
-                    Location.objects.create(name=name, 
+                    
+                    Location.objects.create(id=i,
+                                            name=name, 
                                             description=description,
                                             geometry_precision=geometry_precision,
                                             point_geometry=point_geometry,
                                             real_geometry='',
                                             depth=1,
                                             numchild=0,
-                                            path = i,
+                                            path = '{0:04}'.format(i),
                                             relative_location=location_description,
                                             uuid=uuid)
+                    i+=1
                     
+            print 'first loop completed!'
+            print 'nr. of root nodes are: ' + str(nr_0)
+            
+            i = 1
+#            while sum(maintain_list) is not 0 :
+            for st in maintain_list:
+                if st != 0 :
+                    currentnode = Location.objects.get(pk=i)
+                    #print stad
+                    try :
+                        parentnode = Location.objects.get(uuid=st)
+                        currentnode.save_under(parentnode.pk)
+                    except :
+                        currentnode.path = 'N/A'
+                        currentnode.depth = -999
+                        currentnode.numchild = -999
+                i+=1
+                                
                     
+        print "all completeted~!"
                     
