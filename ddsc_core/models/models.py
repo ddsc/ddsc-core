032ef75d38d8f3e034dd32d9e452471d249dc145
byrman <carsten.byrman@nelen-schuurmans.nl>
2013-02-05 11:05:08 +0100

    Use ETRS89 instead of WGS84

--- ddsc_core/models/models.py
+++ ddsc_core/models/models.py
@@ -11,7 +11,6 @@ from django.core.urlresolvers import reverse
 from django.db import transaction
 from django.db.models.manager import Manager
 from django_extensions.db.fields import UUIDField
-from treebeard.mp_tree import MP_Node
 import networkx as nx
 
 from cassandralib.models import CassandraDataStore
@@ -85,9 +84,9 @@ class Location(BaseModel, MP_Node_ByInstance):
     # The location described in words (exact location is unknown):
     relative_location = models.TextField(null=True, blank=True)
     # A single point that represents the real geometry (e.g. the center):
-    point_geometry = models.PointField(srid=4326, null=True, blank=True)
+    point_geometry = models.PointField(srid=4258, null=True, blank=True)
     # The real geometry (point, linestring, polygon, etc):
-    real_geometry = models.GeometryField(srid=4326, null=True, blank=True)
+    real_geometry = models.GeometryField(srid=4258, null=True, blank=True)
     # Precision in meters with respect to point_geometry:
     geometry_precision = models.FloatField(
         null=True,
@@ -130,7 +129,10 @@ class Location(BaseModel, MP_Node_ByInstance):
                 self.move(parent, pos='first-child')
             else:
                 # node has become a new root node
-                self.move(self.__class__.get_first_root_node(), pos='first-sibling')
+                self.move(
+                    self.__class__.get_first_root_node(),
+                    pos='first-sibling'
+                )
 
         # Reload the instance
         new_self = self.__class__.objects_nosecurity.get(pk=self.pk)
@@ -360,7 +362,7 @@ class LogicalGroup(BaseModel):
 
     # Do not escape HTML-output.
     graph.allow_tags = True
-    
+
     def parents(self):
         return []
 
