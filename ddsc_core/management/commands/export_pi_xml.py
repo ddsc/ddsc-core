af531ed0b9a2b46439c236fea625534df21fd962
byrman <carsten.byrman@nelen-schuurmans.nl>
2013-03-15 16:38:49 +0100

    Improve export of pi.xml

--- ddsc_core/management/commands/export_pi_xml.py
+++ ddsc_core/management/commands/export_pi_xml.py
@@ -2,6 +2,7 @@ from ast import literal_eval
 from datetime import datetime
 from datetime import timedelta
 from optparse import make_option
+import logging
 
 from django.core.management.base import BaseCommand
 from pytz import timezone
@@ -11,6 +12,9 @@ from tslib.readers import PiXmlReader
 from tslib.writers import PiXmlWriter
 
 from ddsc_core.models import Timeseries
+
+logger = logging.getLogger(__name__)
+ISO8601 = "%Y-%m-%dT%H:%M:%SZ"
 
 
 class Command(BaseCommand):
@@ -64,23 +68,43 @@ class Command(BaseCommand):
 
         try:
             utcnow = timezone('UTC').localize(datetime.utcnow())
+            utcnow = timezone('UTC').localize(datetime(2012, 9, 6, 15, 3, 0))
             tznow = utcnow.astimezone(timezone(options.get('timezone')))
             filename = tznow.strftime(options.get('file'))
             destination = open(filename, 'w')
         except TypeError:
             destination = self.stdout
 
-        # start and end may be used to limit the events to be returned.
+        # To limit the events to be returned, `start` and `end` can be used.
+        # They can either be expressed as an ISO 8601 UTC datetime string
+        # or as a timedelta relative to now, for example:
+        #
+        # --start="2012-01-01T00:00:00Z" --end="2013-01-01T00:00:00Z"
+        # --start="{'days': -7}" --end="{}"
 
         start = options.get('start')
+
         if start is not None:
-            start = timedelta(**literal_eval(start))
-            start += utcnow
+            try:
+                start = datetime.strptime(start, ISO8601)
+            except:
+                try:
+                    start = utcnow + timedelta(**literal_eval(start))
+                except:
+                    logger.error("Cannot parse option `start`")
+                    start = None
 
         end = options.get('end')
+
         if end is not None:
-            end = timedelta(**literal_eval(end))
-            end += utcnow
+            try:
+                end = datetime.strptime(end, ISO8601)
+            except:
+                try:
+                    end = utcnow + timedelta(**literal_eval(end))
+                except:
+                    logger.error("Cannot parse option `end`")
+                    end = None
 
         reader = PiXmlReader(source)
         writer = PiXmlWriter(reader.get_tz())
@@ -90,12 +114,13 @@ class Command(BaseCommand):
             try:
                 uuid = md.pop('comment')
                 timeseries = Timeseries.objects.get(uuid=uuid)
-                df = timeseries.get_events()
+                df = timeseries.get_events(start=start, end=end)
                 writer.set_series(md, df)
             except Timeseries.DoesNotExist:
+                logger.warning("Timeseries {0} does not exist".format(uuid))
                 df = pd.DataFrame()
                 writer.set_series(md, df)
-            except:
-                pass
+            except Exception as e:
+                logger.error(e)
 
         writer.write(destination)
