ddf2dd41febcd6601c760d44224dd617e07e46c5
shaoqing <s.lu@fugro.nl>
2013-05-16 11:43:47 +0200

    make it work

--- ddsc_core/management/commands/redo_leftover.py
+++ ddsc_core/management/commands/redo_leftover.py
@@ -2,6 +2,7 @@ from os import listdir
 from os.path import isfile
 from django.core.management.base import BaseCommand
 from ddsc_worker.celery import celery
+from ddsc_core.models.system import Folder
 
 
 class Command(BaseCommand):
@@ -9,17 +10,18 @@ class Command(BaseCommand):
     args = '<path for incoming csv files and socket>'
 
     def handle(self, *args, **options):
-        filePath_file = args[0]
-        filePath_socket = args[1]
-
-        for file in listdir(filePath_file):
-            if isfile(filePath_file + file):
-                celery.send_task(
-                    "ddsc_worker.tasks.new_file_detected", kwargs={
-                    "pathDir": filePath_file,
-                    "fileName": file,
-                    }
-                )
+        filePath_socket = '/mnt/ftp/socket/'
+        filePath_file_obj = Folder.objects.all()
+        for folder in filePath_file_obj:
+            path = folder.path
+            for file in listdir(path):
+                if isfile(path + file):
+                    celery.send_task(
+                        "ddsc_worker.tasks.new_file_detected", kwargs={
+                        "pathDir": path,
+                        "fileName": file,
+                        }
+                    )
 
         for file in listdir(filePath_socket):
             if isfile(filePath_socket + file):
@@ -29,3 +31,4 @@ class Command(BaseCommand):
                     "fileName": file,
                     }
                 )
+        print 'complete!'
